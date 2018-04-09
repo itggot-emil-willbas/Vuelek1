a0227034d8b1c5343afe31820ec6321788276399
Emil Willbas <emil@vildbas.com>
2018-04-09 21:10:49 +0200

    1.1 Canvas mouse
    Cool, mouse, canvas...

--- js/main.js
+++ js/main.js
@@ -2,12 +2,20 @@ new Vue({
     el:"#app",
     data:{
         name:"Emil",
-        addhtmls:'<a href="http://www.google.com">Klicka</>'
+        addhtmls:'<a href="http://www.google.com">Klicka</>',
+        x: 0,
+        y: 0,
+        number:100
     },
     methods:{
         msg(){
             alert("hej");
             // return '<a href="http://www.google.com">Klicka</>';
+        },
+        updateXY(e){
+            console.log(e)
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 })
